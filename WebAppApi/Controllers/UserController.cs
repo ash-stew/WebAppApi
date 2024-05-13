3f77008c35031c60df0fab9fff1924550b91ae95
ash-stew <stewart_ash@hotmail.com>
2024-05-13 16:30:52 +0100

    created crud files

---
+++ WebAppApi/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private static List<string> listUsers = new List<string>() { "Bob", "Bill", "Will", "Dom" };
+
+		[HttpGet]
+		public List<string> GetUsers()
+		{
+			return listUsers;
+		}
+
+		[HttpGet("{id}")]
+		public string GetUser(int id)
+		{
+			if(id >=0 &&  id < listUsers.Count)
+			{
+				return listUsers[id];
+			}
+			return "";
+		}
+
+		[HttpPost]
+		public void AddUser(string username)
+		{
+			listUsers.Add(username);
+		}
+
+		[HttpPut("{id}")]
+		public void UpdateUser(int id, string username)
+		{
+			if(id >=0 && id < listUsers.Count)
+			{
+				listUsers[id] = username;
+			}
+			
+		}
+
+		[HttpDelete("{id}")]
+		public void DeleteUser(int id)
+		{
+			listUsers.RemoveAt(id);
+		}
+	}
+}
